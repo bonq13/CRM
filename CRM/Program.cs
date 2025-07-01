c18100a462579059efba97aedb86df59d4c8a264
Karol <bonq1301@gmail.com>
2025-07-01 19:32:10 +0200

    created basic interface

--- CRM/Program.cs
+++ CRM/Program.cs
@@ -2,8 +2,30 @@
 
 class Program
 {
+    public const string FILE_NAME = "clients.json";
     static void Main(string[] args)
     {
+        Console.WriteLine("Welcome to CRM app!");
+        Console.WriteLine("Please let me know what you would like to do:");
+        Console.WriteLine("1. Add a client");
+        Console.WriteLine("2. Remove a clietn");
+        Console.WriteLine("3. List all clients");
+        Console.WriteLine("4. Edit client data");
+        Console.WriteLine("5. Exit");
+        Console.WriteLine("Choose an option 1 - 5:");
         
+        string choice = Console.ReadLine();
+        Console.WriteLine($"You have choosen option number {choice}.");
+        
+        //stała - uzywamy const
+        //const string FILE_NAME = "clients.json";
+        
+        //zmienna - bez const
+        int choosenOption = int.Parse(choice);  
+        
+        Console.WriteLine("Please choose another option 1 - 5:");
+        choice = Console.ReadLine();
+        Console.WriteLine($"You have choosen option number {choice}.");
+        choosenOption = int.Parse(choice);  
     }
 }
--- CRM/obj/Debug/net9.0/CRM.AssemblyInfo.cs
+++ CRM/obj/Debug/net9.0/CRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e346b5b2fc80b4dfcdbf971814874097bd34ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
